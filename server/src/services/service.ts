bbe6e18f039fcbe850823cd74d5a99185a00a028
Muammer <wwebmaster.kls@gmail.com>
2024-12-11 11:39:34 +0300

    init and add files

---
+++ server/src/services/service.ts
@@ -0,0 +1,9 @@
+import type { Core } from '@strapi/strapi';
+
+const service = ({ strapi }: { strapi: Core.Strapi }) => ({
+  getWelcomeMessage() {
+    return 'Welcome to Strapi Excel Export 2025 Plugin 🚀';
+  },
+});
+
+export default service;
